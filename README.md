ab3127e875f1d8cd208d14f9a0d5142998f2b082
bradenkwebb <bkwebb23@gmail.com>
2024-03-27 00:12:42 -0400

    allow Buzz CLI commands to be called in background

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ pip install -r requirements.txt
 ```
 
 ## Usage
+
+
+## Notes
+I originally planned to use Azure Cognitive Services to transcribe the podcast audio, at least for simplicity while testing. The free tier only allows 5 hours of audio per month, so I'd fly through that really quickly if I were to use this tool regularly.
+
+However, it doesn't appear as though Azure actually provides a convenient way to *align* its speech-to-text outputs with audio. That seems like a major oversight—there are lots of non-neural forced alignment tools out there—but [Buzz](https://chidiwilliams.github.io/buzz/docs) actually provides a great solution, which calls on OpenAI's Whisper model (which was actually open-sourced). Since I can run that locally (albeit somewhat slowly) from a very straightforward CLI, I think I'll use that instead.
+
+Overall process
+- Every time a new podcast episode is released, download it 
--- skipcastify/audio_processor.py
+++ skipcastify/audio_processor.py
@@ -1,66 +1,37 @@
-import requests
-import librosa
 from pydub import AudioSegment
 from pydub.playback import play
 import sys
 from rss_parser import fetch_podcast_updates
 import logging
-import azure.cognitiveservices.speech as speechsdk
 import os
+import time
 l = logging.getLogger("pydub.converter")
 l.setLevel(logging.WARN)
 l.addHandler(logging.StreamHandler())
 
-
-
-# speech_key, service_region = os.environ['SPEECH_KEY'], os.environ['SPEECH_REGION']
-# speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
-
 channel_url = "https://lexfridman.com/feed/podcast/"
 
-# feed = fetch_podcast_updates(channel_url)
-# url = feed.entries[0]['links'][1]['href']
-# print(url)
-
-# response = requests.get(url)
-# print("Content is a", str(int(response.headers.get('Content-Length')) // 1024 // 1024), "MB", response.headers.get('Content-Type'), 'file')
-# print("Last modified", response.headers['Last-Modified'])
-
-# with open('temp.mp3', 'wb') as f:
-#     f.write(response.content)
 aud = AudioSegment.from_mp3('temp.mp3')
-f_10_sec = aud[10 * 1000:20 * 1000]
-f_10_sec.export('short.wav', format='wav')
+# f_10_sec = aud[10 * 1000:20 * 1000]
+# f_10_sec.export('short.wav', format='wav')
+f_10_min = aud[:10 * 60 * 1000]
 
+f_path = 'data/intro.wav'
+f_10_min.export(f_path, format='wav')
 
-# aud = AudioSegment.from_mp3('short.mp3')
+import subprocess
+# this at least runs in the background now, so I can do other things...
+process = subprocess.Popen(['buzz', 'add', '--task', 'transcribe', '--srt', '--vtt', '--txt', f_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-# transcript = 'Business of fighting and Dana is truly the mastermind behind the UFC.'
+# Fix this!! Very hacky
+while process.poll() is None:
+    # Process is still running, you can do other tasks here if needed
+    if os.path.exists(f_path):
+        try:
+            os.rename(f_path, f_path)
+            print('Access on file "' + f_path +'" is available!')
+        except OSError as e:
+            print('Access-error on file "' + f_path + '"! \n' + str(e))
+    time.sleep(1)  # Adjust the sleep duration as needed
 
-# import requests
-# import librosa
-# from pydub import AudioSegment
-# from pydub.playback import play
-# import sys
-# from skipcastify.rss_parser import fetch_podcast_updates
-# import logging
-# import azure.cognitiveservices.speech as speechsdk
-# import os
-
-# speech_key, service_region = os.environ['SPEECH_KEY'], os.environ['SPEECH_REGION']
-# speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
-# audio_config = speechsdk.AudioConfig(filename='short.wav')
-# speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
-
-# result = speech_recognizer.recognize_once()
-
-# # Checks result.
-# if result.reason == speechsdk.ResultReason.RecognizedSpeech:
-#     print("Recognized: {}".format(result.text))
-# elif result.reason == speechsdk.ResultReason.NoMatch:
-#     print("No speech could be recognized: {}".format(result.no_match_details))
-# elif result.reason == speechsdk.ResultReason.Canceled:
-#     cancellation_details = result.cancellation_details
-#     print("Speech Recognition canceled: {}".format(cancellation_details.reason))
-#     if cancellation_details.reason == speechsdk.CancellationReason.Error:
-#         print("Error details: {}".format(cancellation_details.error_details))
+print("Done!")
