b564505667d683db4e672e652a8a7bd4caa94eb2
Braden Webb <bkwebb23@gmail.com>
2025-06-28 16:50:29 -0400

    handle links that appear in a list

--- skipcastify/services/generate_feeds.py
+++ skipcastify/services/generate_feeds.py
@@ -9,13 +9,15 @@ import logging
 logger = logging.getLogger(__name__)
 
 load_dotenv()
-server_ip = os.environ.get('SERVER_IP')  # Replace with your server IP
-data_dir = os.environ.get('DATA_DIR')
+server_ip = os.environ.get("SERVER_IP")  # Replace with your server IP
+data_dir = os.environ.get("DATA_DIR")
+
 
 def slugify(title):
-    return re.sub(r'[^a-z0-9]+', '-', title.lower()).strip('-')
+    return re.sub(r"[^a-z0-9]+", "-", title.lower()).strip("-")
 
-with open(os.environ.get('SUBSCRIPTIONS')) as f:
+
+with open(os.environ.get("SUBSCRIPTIONS")) as f:
     config = yaml.safe_load(f)
 
 for subscription_url in config["subscriptions"]:
@@ -26,7 +28,7 @@ for subscription_url in config["subscriptions"]:
     fg = FeedGenerator()
     fg.title(title)
     fg.link(href=subscription_url)
-    fg.description(feed.feed.get('description', ''))
+    fg.description(feed.feed.get("description", ""))
 
     for entry in feed.entries[:10]:  # take top 10 episodes
         fe = fg.add_entry()
@@ -35,7 +37,10 @@ for subscription_url in config["subscriptions"]:
         ep_filename = slug + "-" + entry.title.replace(" ", "_") + ".mp3"
 
         try:
-            fe.link(href=entry.link)
+            if hasattr(entry, "link"):
+                fe.link(href=entry.link)
+            else:
+                fe.link(href=getattr(entry.links[0], "href"))
         except AttributeError as e:
             logger.warning(f"Error with {entry.title} from podcast {title}")
             logger.warning(e)
@@ -43,7 +48,7 @@ for subscription_url in config["subscriptions"]:
             fe.enclosure(
                 url=f"http://{server_ip}/episodes/{slug}/{ep_filename}",
                 type="audio/mpeg",
-                length=entry.enclosures[0]['length']
+                length=entry.enclosures[0]["length"],
             )
         except AttributeError as e:
             logger.warning(f"Error with {entry.title} from podcast {title}")
